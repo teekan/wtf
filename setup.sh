c5a224aaeecf0fb54c7ed2167747b303f14728e0
teekan <82342440+teekan@users.noreply.github.com>
2021-04-15 07:38:14 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ apt install -y firefox python3-pip
 pip3 install selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.29.1/geckodriver-v0.29.1-linux64.tar.gz
 tar -xf geckodriver-v0.29.1-linux64.tar.gz
-python3 code.py
+python3 run.py
