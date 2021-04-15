483a6b5c1a168b40dde5965cb68a95fffc646c14
teekan <82342440+teekan@users.noreply.github.com>
2021-04-15 07:50:27 -0700

    Update run.py

--- run.py
+++ run.py
@@ -5,5 +5,23 @@ opts.headless = True
 def hit():
   try:
     browser = webdriver.Firefox(options=opts,'/home/ubuntu/geckodriver')
+    try:
+      browser.get("http://www.slutbags.tk")
+      try:
+        browser.find_elements_by_tag_name('body')[0].click()
+        try:
+          browser.switch_to.window(browser.window_handles[1])
+          for x in range(len(browser.find_elements_by_tag_name('iframe'))):
+            browser.find_elements_by_tag_name('iframe')[x].click()
+        except Exception as e:
+          print(e)
+          print("Failed clicking ads")
+      except Exception as e:
+        print(e)
+        print("Failed clicking popunder")
+    except Expection as e:
+      print(e)
+      print("Failed getting page")
   except Exception as e:
     print(e)
+    print("Failed loading browser")
